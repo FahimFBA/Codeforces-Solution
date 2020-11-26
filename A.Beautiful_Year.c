5e9325e842867908ce3bf1226b25c61ce40910b2
FahimFBA <fahimbinamin@gmail.com>
2020-11-26 23:37:05 +0600

    added another solve of mine

---
+++ A.Beautiful_Year.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    scanf("%d" , &year);
+    for (;;)
+    {
+        year++;
+        int a=year/1000;
+        int b=(year/100)%10;
+        int c=(year/10) % 10;
+        int d=(year%10);
+        if (a!=b && a!=c && a!=d && b!=c && b!=d && c!=d)
+        {
+            break;
+        }
+
+    }
+    printf("%d\n" , year);
+}
