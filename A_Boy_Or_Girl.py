64363636df66b338d9adf05d5c3bea79dce766e0
FahimFBA <fahimbinamin@gmail.com>
2021-03-14 03:30:35 +0600

    Boy or Girl - Solvedgit add .

---
+++ A_Boy_Or_Girl.py
@@ -0,0 +1,12 @@
+def main():
+    s1 = input()  # String.
+    s1 = dict.fromkeys(s1)  # Convert it to dictionary to remove duplicates.
+
+    if len(s1) % 2 == 0:
+        print("CHAT WITH HER!")
+    else:
+        print("IGNORE HIM!")
+
+
+if __name__ == "__main__":
+    main()
