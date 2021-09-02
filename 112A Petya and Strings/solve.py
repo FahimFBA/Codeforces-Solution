a6acbf5e215aa752496b453a5ac039e4f0ced7e6
FahimFBA <fahimbinamin@gmail.com>
2021-09-03 01:51:35 +0600

    solved

---
+++ 112A Petya and Strings/solve.py
@@ -0,0 +1,4 @@
+str1 = input().lower() # Taking a string as input from the user, converting the given string into lowercase and returns the string.
+str2 = input().lower() # Taking a string as input from the user, converting the given string into lowercase and returns the string.
+result = ((str1>str2) - (str1<str2))
+print(result)
