69dccba2c4baaad7a49aae2e14a7bbf053d37fcb
FahimFBA <fahimbinamin@gmail.com>
2021-09-10 22:26:43 +0600

    solved

---
+++ 617A Elephant/solve.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n % 5 == 0)
+    {
+        printf("%d\n", (n / 5));
+    }
+    else
+    {
+        printf("%d\n", ((n / 5) + 1));
+    }
+}
