4b452b243e892791ff2079828a53adc28aab51f8
FahimFBA <fahimbinamin@gmail.com>
2021-10-11 20:57:56 +0600

    solved

---
+++ 58A Chat Room/solve.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[100];
+    char str2[100] = "hello";
+    scanf("%s", &str1);
+    int i, j = 0, count = 0;
+    int len = strlen(str1);
+    for (i = 0; i < len; i++)
+    {
+        if (str1[i] == str2[j])
+        {
+            j++;
+            count++;
+        }
+    }
+    if (count == 5)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+}
