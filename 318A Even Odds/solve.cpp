e97c135173926372452c12d57dd1aec6ef291326
FahimFBA <fahimbinamin@gmail.com>
2021-10-18 21:43:51 +0600

    solved

---
+++ 318A Even Odds/solve.cpp
@@ -0,0 +1,27 @@
+// Pattern:
+
+//  if (k <= (n + 1) / 2)
+//     {
+//         cout << k * 2 - 1 << endl;
+//     }
+//     else
+//     {
+//         cout << (k - (n + 1) / 2) * 2 << endl;
+//     }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, k;
+    cin >> n >> k;
+    if (k <= (n + 1) / 2)
+    {
+        cout << k * 2 - 1 << endl;
+    }
+    else
+    {
+        cout << (k - ((n + 1) / 2)) * 2 << endl;
+    }
+}
