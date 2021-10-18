a31ddd644d84432ee6b0edc36f0e242053fa46a8
FahimFBA <fahimbinamin@gmail.com>
2021-10-18 21:17:58 +0600

    solved

---
+++ 1030A In Search of an Easy Problem/solve.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, i, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        if (n == 1)
+        {
+            cout << "HARD" << endl;
+            return 0;
+        }
+    }
+    cout << "EASY" << endl;
+}
