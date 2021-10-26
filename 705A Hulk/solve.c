091d4a4a525011502167f1d7e544160d4088232f
FahimFBA <fahimbinamin@gmail.com>
2021-10-26 15:10:46 +0600

    solved

---
+++ 705A Hulk/solve.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    printf("I hate");
+    for (i = 1; i < n; i++)
+    {
+        printf(i & 1 ? " that I love" : " that I hate");
+    }
+    printf(" it");
+    return 0;
+}
