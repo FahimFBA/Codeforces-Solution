c5350cf0d7a4facfe0081f0382531bfbc780f068
FahimFBA <fahimbinamin@gmail.com>
2022-07-10 08:29:53 +0600

    solved

---
+++ 1335A Candies and Two Sisters/Solve.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, value;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        cin >> value;
+        cout << (value - 1) / 2 << endl;
+    }
+}
